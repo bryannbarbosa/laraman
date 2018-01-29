9ce9c4be1440f9ab0e8c8a3f9a31d987852378eb
bryannbarbosa <bryannbarbosa@gmail.com>
2018-01-29 17:11:04 -0200

    fixing the problem of middlewares and refactoring code

--- src/ExportRoutesToPostman.php
+++ src/ExportRoutesToPostman.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace RLStudio\Laraman;
 
 use Illuminate\Routing\Router;
@@ -14,29 +13,25 @@ class ExportRoutesToPostman extends Command
      *
      * @var string
      */
-    protected $signature = 'laraman:export {--name=laraman-export} {--port=8000}';
-
+    protected $signature = 'laraman:export {--name=laraman-export} {--match=} {--port=8000}';
     /**
      * The console command description.
      *
      * @var string
      */
     protected $description = 'Export all routes to a json file that can be imported in Postman';
-
     /**
      * The Laravel router.
      *
      * @var \Illuminate\Routing\Router
      */
     private $router;
-
     /**
      * The filesystem implementation.
      *
      * @var \Illuminate\Contracts\Filesystem\Filesystem
      */
     private $files;
-
     /**
      * Create a new command instance.
      *
@@ -49,7 +44,6 @@ class ExportRoutesToPostman extends Command
         $this->files = $files;
         parent::__construct();
     }
-
     /**
      * Execute the console command.
      *
@@ -57,51 +51,82 @@ class ExportRoutesToPostman extends Command
      */
     public function handle()
     {
-        $pattern = $this->option('search');
         $name = $this->option('name');
         $port = $this->option('port');
+        $search = $this->option('match');
         // Set the base data.
         $routes = [
-              'variables' => [],
-              'info' => [
-                  'name' => $name,
-                  '_postman_id' => Uuid::uuid4(),
-                  'description' => '',
-                  'schema' => 'https://schema.getpostman.com/json/collection/v2.1.0/collection.json',
-              ]
-          ];
-
+            'variables' => [],
+            'info' => [
+                'name' => $name,
+                '_postman_id' => Uuid::uuid4(),
+                'description' => '',
+                'schema' => 'https://schema.getpostman.com/json/collection/v2.1.0/collection.json',
+            ]
+        ];
         foreach ($this->router->getRoutes() as $route) {
             foreach ($route->methods as $method) {
                 if ($method == 'HEAD') {
                     continue;
                 }
-                $uri = $route->uri();
-                if($uri == '/') {
-                  $uri = null;
+
+                $alias = $route->uri();
+
+                if ($route->uri() == '/') {
+                    $uri = null;
+                } else {
+                    $uri = $route->uri();
                 }
-                $url = url()->full();
-                $url .= ':' . $port . '/' . $uri;
-                $routes['item'][] = [
-                          'name' => $method.' '.$route->uri(),
-                          'request' => [
-                              'url' => $url,
-                              'method' => strtoupper($method),
-                              'header' => [
-                                  [
-                                      'key' => 'Content-Type',
-                                      'value' => 'application/json',
-                                      'description' => ''
-                                  ]
-                              ],
-                              'body' => [
-                                  'mode' => 'raw',
-                                  'raw' => '{\n    \n}'
-                              ],
-                              'description' => '',
+
+                $server = url()->current();
+                $url = "{$server}:${port}/${uri}";
+
+                if ($search) {
+                    if (in_array($search, $route->middleware())) {
+                        $routes['item'][] = [
+                        'name' => "${method} ${alias}",
+                        'request' => [
+                            'url' => $url,
+                            'method' => strtoupper($method),
+                            'header' => [
+                                [
+                                    'key' => 'Content-Type',
+                                    'value' => 'application/json',
+                                    'description' => ''
+                                ]
+                            ],
+                            'body' => [
+                                'mode' => 'raw',
+                                'raw' => '{\n    \n}'
+                            ],
+                            'description' => '',
+                        ],
+                        'response' => [],
+                    ];
+                  }
+                }
+                if (!$search) {
+                    $routes['item'][] = [
+                      'name' => "${method} ${alias}",
+                      'request' => [
+                          'url' => $url,
+                          'method' => strtoupper($method),
+                          'header' => [
+                              [
+                                  'key' => 'Content-Type',
+                                  'value' => 'application/json',
+                                  'description' => ''
+                              ]
                           ],
-                          'response' => [],
-                        ];
+                          'body' => [
+                              'mode' => 'raw',
+                              'raw' => '{\n    \n}'
+                          ],
+                          'description' => '',
+                      ],
+                      'response' => [],
+                  ];
+                }
             }
         }
         if (! $this->files->put($name.'.json', json_encode($routes))) {
